f8abd8d30fe3032e01aa80d574fdc209214994ce
Task Nagashige <task.19.xx@gmail.com>
2020-02-26 11:10:12 +0900

    middlewareによるグローバルフックの登録

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,11 @@ module.exports = {
   ** Customize the progress bar color
   */
   loading: { color: '#3B8070' },
+  router: {
+    middleware: [
+      'auth'
+    ]
+  },
   /*
   ** Build configuration
   */
